0133a4105f01b25f1c1ee6cd21680642c3eaf3e8
gracelsy <43689473+gracelsy@users.noreply.github.com>
2025-05-20 15:52:23 +0800

    Update fonts.ts
    修改默认字体

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 export const zpix = localFont({
-  src: '../public/fonts/AaHuDieXingHuan-YinHeShiJi.ttf',
+  src: '../public/fonts/huangkaihuaLawyerfont-2.ttf',
   variable: '--font-zpix',
   display: 'swap',
 }) 
