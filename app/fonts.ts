0758109d9154aa49d9c249bc3b8fee1b8189732e
gracelsy <43689473+gracelsy@users.noreply.github.com>
2025-05-20 16:12:12 +0800

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 export const zpix = localFont({
-  src: '../public/fonts/huangkaihuaLawyerfont-2.ttf',
+  src: '../public/fonts/HongLeiZhuoShu.ttf',
   variable: '--font-zpix',
   display: 'swap',
 }) 
