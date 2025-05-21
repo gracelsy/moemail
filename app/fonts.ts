a86e1825b9b97a584a0955b6f93a23e026c4f6ec
gracelsy <43689473+gracelsy@users.noreply.github.com>
2025-05-21 14:31:48 +0800

    Update fonts.ts

--- app/fonts.ts
+++ app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from 'next/font/local'
 
 export const zpix = localFont({
-  src: '../public/fonts/FangZhengFangSongJianTi-1.ttf',
+  src: '../public/fonts/上图东观体-细体.ttf',
   variable: '--font-zpix',
   display: 'swap',
 }) 
